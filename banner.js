374d9f1d8370d1e0a86bdc1caf3c9425bc08bc76
Zcy <544641781@qq.com>
2016-09-04 19:32:02 +0800

    first update

---
+++ banner.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Administrator on 2016/9/3 0003.
+ */
+window.onload = function () {
+    var oList = document.getElementById('list');
+    var oHjs_imgCont = document.getElementById('hjs_imgCont');
+    var oScorllBar = document.getElementById('scorllBar').getElementsByTagName('a');
+    var oNext_bnt = document.getElementById('next_bnt');
+    var oPrev_bnt = document.getElementById('prev_bnt');
+    var index = 1;
+    var len = 6;
+    var interval = 3000;
+    var timer;
+
+    oNext_bnt.onclick = function () {
+        if (index == 6) {
+            index = 1;
+        }
+        else {
+            index += 1;
+        }
+        showButton();
+        animate(-100);
+
+    };
+    oPrev_bnt.onclick = function () {
+        if (index == 1) {
+            index = 6;
+        }
+        else {
+            index -= 1;
+        }
+        showButton();
+        animate(100);
+    };
+    function animate(offset) {
+        var left = parseInt(oList.style.left) + offset;
+        var time = 300;
+        var interval = 10;
+        var speed = offset/(time/interval);
+
+        function go() {
+            if ((speed < 0 && parseInt(oList.style.left) > left) || (speed > 0 && parseInt(oList.style.left) < left)) {
+                oList.style.left = parseInt(oList.style.left) + speed + '%';
+                setTimeout(go,interval);
+            }
+            else {
+                oList.style.left = left + '%';
+                if (left < -600) {
+                    oList.style.left = -100 + '%';
+                }
+                if (left > -100) {
+                    oList.style.left = -600 + '%';
+                }
+            }
+
+        }
+        go();
+    }
+    function showButton() {
+        for (var i = 0; i < oScorllBar.length; i++)
+            if (oScorllBar[i].className == 'active') {
+                oScorllBar[i].className = '';
+                break;
+            }
+        oScorllBar[index - 1].className = 'active';
+    }
+
+    for (var i = 0; i < oScorllBar.length; i++) {
+        oScorllBar[i].index=i+1;
+        oScorllBar[i].onclick = function () {
+            var myIndex = this.index;
+            var offset = -100 * (myIndex-index);
+            index = myIndex;
+            showButton();
+            animate(offset);
+        }
+    }
+}
+
