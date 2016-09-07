c4ebc66f89172be891d3ab55ff5f6d7cc03dd9c5
Zcy <544641781@qq.com>
2016-09-07 17:40:00 +0800

    first update

--- nav.js
+++ nav.js
@@ -2,17 +2,51 @@
  * Created by Administrator on 2016/9/4 0004.
  */
 function nav() {
-    var oMenu = document.getElementById('menu').getElementsByTagName('a');
+    var oMenu = document.getElementsByClassName('channel');
+    var oUla = document.getElementsByClassName('ula');
+    var oLia = document.getElementsByClassName('lia');
+    var len= oMenu.length;
+    var timer = null;
     function menu() {
-        for (i = 1; i < (oMenu.length); i++) {
+        for (var i= 0; i < len; oMenu[i].id = i,oLia[i].id=i, i++) {
             oMenu[i].onmouseover = function () {
-                this.className = 'hover';
+                var that = this;
+                if(timer){
+                    clearTimeout(timer);
+                }
+                if (!(that.className == 'channel hovered')) {
+                    that.className = 'channel hover';
+                }
+                timer=setTimeout(function () {
+                    oUla[that.id].style.display = 'block';
+                },500);
             };
+
             oMenu[i].onmouseout = function () {
-                this.className = 'hover1';
+                var that = this;
+                if (!(that.className == 'channel hovered')) {
+                    that.className = 'channel hover1';
+                }
+            };
+
+            oLia[i].onmouseout = function () {
+                var that = this;
+                if(timer) {
+                    clearTimeout(timer);
+                    oUla[that.id].style.display = 'none';
+                }
+            };
+
+            oMenu[i].onclick = function () {
+                if (!(this.className == 'channel hovered')) {
+                    for (var j = 0; j < len; j++) {
+                        oMenu[j].className = 'channel hover1';
+                    }
+                }
+                this.className = 'channel hovered';
             };
         }
     }
     menu();
-};
+}
 
